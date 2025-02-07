c1cef9a2054c3ad808aeb28c4747dc91febe6e02
Flexerin0 <vfieliks@mail.ru>
2025-02-07 14:43:04 +0300

    Updated

---
+++ makefile_test/makefile_test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace makefile_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
